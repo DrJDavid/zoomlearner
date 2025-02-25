45cb8bf844673447aa2b95cd43298bf41c6fbf62
David <davidrjohnson99@gmail.com>
2025-02-25 03:08:04 -0600

    config change

--- public/env-config.js
+++ public/env-config.js
@@ -3,3 +3,4 @@ window.__ENV = window.__ENV || {};
 window.__ENV.VITE_SUPABASE_URL = '';
 window.__ENV.VITE_SUPABASE_ANON_KEY = '';
 console.log('Runtime environment variables loaded successfully');
+// Add this line to check if the environment variables are available
